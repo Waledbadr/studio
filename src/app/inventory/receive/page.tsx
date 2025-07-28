83c0c4f3dd3b8cad61965d10960a88bbcb0191e2
Mohammed Alabdali <abdalim2@gmail.com>
2025-07-28 06:28:59 +0000

    Confirm Receipt & Update Stock

--- src/app/inventory/receive/page.tsx
+++ src/app/inventory/receive/page.tsx
@@ -41,6 +41,7 @@ export default function ReceiveMaterialsPage() {
         Array.from({ length: 3 }).map((_, i) => (
              <TableRow key={`skeleton-${i}`}>
                 <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                 <TableCell><Skeleton className="h-5 w-40" /></TableCell>
                 <TableCell><Skeleton className="h-5 w-16" /></TableCell>
                 <TableCell><Skeleton className="h-6 w-24 rounded-full" /></TableCell>
@@ -111,3 +112,6 @@ export default function ReceiveMaterialsPage() {
         </div>
     )
 }
+
+
+    
